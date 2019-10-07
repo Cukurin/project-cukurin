061d45a6d4b2efbc3a935cd6939bc086397b1477
nicohusin <nicohusin@gmail.com>
2019-10-07 15:08:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import SignUp from './components/signup'
 import './App.css';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core'
+import {amber} from '@material-ui/core/colors'
+import NavTabs from './components/navbar'
+
+const theme = createMuiTheme({
+  palette: {
+    primary : amber
+  }
+})
 
 function App() {
   return (
+    <MuiThemeProvider theme = {theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavTabs/>
+      <SignUp />
     </div>
+    </MuiThemeProvider>
   );
 }
 
